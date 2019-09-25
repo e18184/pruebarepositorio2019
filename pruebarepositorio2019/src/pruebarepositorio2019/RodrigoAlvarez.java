6046288c4865dd4306d377450164c134653628c7
alumno <alumno@alumno-virtual-machine>
2019-09-25 11:57:01 -0700

    14:56 25-09-2019

---
+++ pruebarepositorio2019/src/pruebarepositorio2019/RodrigoAlvarez.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebarepositorio2019;
+
+/**
+ *
+ * @author alumno
+ */
+public class RodrigoAlvarez {
+    public static void main(String[] args) {
+        System.out.println("Rodrigo Alvarez");
+    }
+
+}
