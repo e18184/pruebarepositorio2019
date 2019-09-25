7572b842144430fb37cbe0431e9c9e1979d01a93
LENOVO <LENOVO@DESKTOP-KAMR8T5>
2019-09-25 15:14:56 -0400

    modificando codigo en una nueva clase

---
+++ pruebarepositorio2019/src/pruebarepositorio2019/SamuelSanchez.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebarepositorio2019;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SamuelSanchez {
+     public static void main(String[] args) {
+        
+        System.out.println("Creacion de una nueva clase");
+       
+    }
+}
